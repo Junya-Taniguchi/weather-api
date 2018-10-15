e2763ddfdd14968511760e05586b8c01f0d7ae71
谷口純也 <taniguchi@taniguchijunya-no-iMac.local>
2018-10-15 18:13:46 +0900

    second commit

--- weather.py
+++ weather.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-api = "http://api.openweathermap.org/data/2.5/weather?q=London&APPID=6006c8a1d90dd8e603fc35057667698f"
+api = "http://api.openweathermap.org/data/2.5/weather?q=London&APPID=APIkey"
 r = requests.get(api)
 data = json.loads(r.text)
 print("都市"+ ":" + data["name"])
